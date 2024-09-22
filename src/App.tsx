f6e9485821d3d8b022fae8c402f151aad272572c
evgenyprymak <prymakev@gmail.com>
2024-09-22 18:03:25 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,18 +8,11 @@ function App() {
   return (
     <>
 
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>yo</h1>
+      <div className='logo'/>
+      <div className="card" />
+
+
     </>
   )
 }
