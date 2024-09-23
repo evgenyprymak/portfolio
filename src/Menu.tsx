11353a8b662779ea4f539885a25efa0ef2c8a5cb
evgenyprymak <prymakev@gmail.com>
2024-09-23 14:49:26 +0200

    sep23-2

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -7,7 +7,7 @@ function Menu() {
         <div className="menu">
           <div className='menu-item'>Works</div>
           <div className='menu-item'>Procedural Art</div>
-          <div className='menu-item'>About</div>
+          <div className='menu-item'>About Me</div>
         </div>
       </>
     )
