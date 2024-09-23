bceb08f677e8b5424ed6fde47c042770f6f297ad
evgenyprymak <prymakev@gmail.com>
2024-09-23 13:28:00 +0200

    menu

---
+++ src/Menu.tsx
@@ -0,0 +1,16 @@
+import './Menu.css'
+
+function Menu() {
+  
+    return (
+      <>
+        <div className="menu">
+          <div className='menu-item'>Works</div>
+          <div className='menu-item'>Procedural Art</div>
+          <div className='menu-item'>About</div>
+        </div>
+      </>
+    )
+  }
+  
+  export default Menu
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import Menu from './Menu.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Menu />
     <App />
   </StrictMode>,
 )
