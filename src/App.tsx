28406f015deb08cf24bbe3db39f1bc4c1c240c46
evgenyprymak <prymakev@gmail.com>
2024-09-23 13:10:33 +0200

    fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,7 +7,7 @@ function App() {
   return (
     <>
 
-      <h1>Vite + React</h1>
+      <h1 className='test' >Vite 11 React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
