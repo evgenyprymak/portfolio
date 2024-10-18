0c10e4cb2c7d5fc4a8bb4afc790f0a5862c90220
evgenyprymak <prymakev@gmail.com>
2024-10-18 17:49:21 +0200

    progress 34

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -10,9 +10,9 @@ const Menu = () => {
       <div className="top-bar" ref={topBarRef}>
         <div className='logo'><a href='#/'>Evgeny Prymak</a></div>
         <div className='menu'>
-          <a href='#'><div className="menu-item link link--metis">Product Design</div></a>
-          <a href='#'><div className="menu-item link link--metis">Procedural Art</div></a>
-          <a href='#'><div className="menu-item link link--metis">Resume</div></a>
+          {/* <a href='#'><div className="menu-item link link--metis">Product Design</div></a>
+          <a href='#'><div className="menu-item link link--metis">Procedural Art</div></a> */}
+          <a href='https://drive.google.com/file/d/1u-RuCyojZ77YW-6CqajPDBz1JkzBudT7/view' target="_blank"><div className="menu-item link link--metis">Resume</div></a>
         </div>
       </div>
     </div>
