5dc81b2d95c9fa873109196e97a1bd095a068012
evgenyprymak <prymakev@gmail.com>
2024-10-21 18:50:27 +0200

    Link update

--- src/ProjectDetailsProcedural.tsx
+++ src/ProjectDetailsProcedural.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { motion, AnimatePresence } from "framer-motion"
 import PageHero from "./components/PageHero";
 import Task from "./components/Task";
