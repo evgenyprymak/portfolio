93756f7415e08c794a72694724b03fe90ffdbee6
evgenyprymak <prymakev@gmail.com>
2025-10-13 21:01:19 +0200

    chore: set base/homepage for portfolio-v2 deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio/', 
+  base: '/portfolio-v2/', 
   plugins: [react()],
   build: {
     outDir: 'dist',
