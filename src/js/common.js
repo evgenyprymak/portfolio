6975531130e20e81d74ffffb9b6062aa68bcabdb
evgenyprymak <prymakev@gmail.com>
2025-10-14 00:02:44 +0200

    chore: remove unused src files (prune project to MainPage)

--- src/js/common.js
+++
@@ -1,9 +0,0 @@
-export const debounce = (func, delay) => {
-    let timerId; // Holds a reference to the timeout between calls.
-    return (...args) => {
-      clearTimeout(timerId); // Clears the current timeout, if any, to reset the debounce timer.
-      timerId = setTimeout(() => {
-          func.apply(this, args); // Calls the passed function after the specified delay with the correct context and arguments.
-      }, delay);
-    };
-  };
