ed8cb2e58b0d182edd31a185acb2153d3a863e4f
evgenyprymak <prymakev@gmail.com>
2026-01-29 23:10:47 +0100

    Your commit message

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -16,6 +16,7 @@ interface MenuProps {
 const Menu: React.FC<MenuProps> = ({ productDesign, proceduralArt, contact, }) => {
   const topBarRef = useRef<HTMLDivElement>(null);
   const [compact, setCompact] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   useEffect(() => {
     const handleScroll = () => {
@@ -36,21 +37,19 @@ const Menu: React.FC<MenuProps> = ({ productDesign, proceduralArt, contact, }) =
         className={`top-bar ${compact ? 'compact' : ''}`}
         ref={topBarRef}
       >
-        <div className={`menu-wrapper ${compact ? 'compact' : ''}`}>
-          <div className='menu menu-item link' style={{fontWeight:'700'}}>Menu
-            <div className={`menu-dropdown`}>
-              <div className={`menu-item link ${productDesign ? 'active' : ''}`}><Link to="/" className=''>Product Design & Web</Link></div>
-              <div className={`menu-item link ${proceduralArt ? 'active' : ''}`}><Link to="/project/100" className=''>Procedural Art</Link></div>
-              <div className={`menu-item link ${contact ? 'active' : ''}`}><Link to="/contact" className=''>Contacts</Link></div>
+        <div className={`menu-wrapper ${compact ? 'compact' : ''} ${isMenuOpen ? 'menu-open' : ''}`}>
+          <div className='menu-container'>
+            <div className='menu menu-item nav-link' style={{zIndex: 21}} onClick={() => setIsMenuOpen(!isMenuOpen)}>Menu</div>
+            <div className={`menu-dropdown ${isMenuOpen ? 'open' : ''} ${compact ? 'compact' : ''}`}>
+              <div id='menu-divider' className={`${compact ? 'compact' : ''}`}></div>
+              <div className={`menu-item ${productDesign ? 'active' : ''}`}><Link to="/" className='nav-link'>Product Design & Web</Link></div>
+              <div className={`menu-item ${proceduralArt ? 'active' : ''}`}><Link to="/project/100" className='nav-link'>Procedural Art</Link></div>
+              <div className={`menu-item ${contact ? 'active' : ''}`}><Link to="/contact" className='nav-link'>Contacts</Link></div>
             </div>
-            {/* <div className={`'menu-item link ${productDesign ? 'active' : ''}`}><Link to="/" className=''>Product Design & Web</Link></div>
-            <div className={`'menu-item link ${proceduralArt ? 'active' : ''}`}><Link to="/project/100" className=''>Procedural Art</Link></div>
-            <div className={`'menu-item link ${contact ? 'active' : ''}`}><Link to="/contact" className=''>Contacts</Link></div> */}
-
           </div>
           <div className='logo'><a href='#/'>Evgeny Prymak</a></div>
           <div className='cta-button'>
-            <a href='https://drive.google.com/file/d/1rJz_sODrLDV9r8N9x5bvYyeduQOEyB-k/view' target="_blank"><div className="menu-item link">Resume</div></a>
+            <a href='https://drive.google.com/file/d/1rJz_sODrLDV9r8N9x5bvYyeduQOEyB-k/view' target="_blank"><div className="menu-item nav-link">Resume</div></a>
           </div>
         </div>
 
